8796220a3e07a79ec5df48fd20a15b16e977556d
Prakhar Chatterjee <pchatterjee21@gmail.com>
2020-01-10 15:34:08 +0530

    Pushing code changes

--- TechAssesment/src/test/java/commonUtil/API_AutoCommon.java
+++ TechAssesment/src/test/java/commonUtil/API_AutoCommon.java
@@ -23,9 +23,8 @@ import junit.framework.Assert;
 public class API_AutoCommon {
 
 	String url = "http://api-aws-eu-qa-1.auto1-test.com";
-	String url_manufacturer = "v1/car-types/manufacturer?wa_key=coding-puzzle-client-449cc9d&locale=en";
-	Map<String,String> manufacturer_Map = new HashMap<String,String>();
-	Set<String> manufacturer_code = new HashSet<String>();
+	private static Set<String> manufacturerCode = new HashSet<String>();
+	private static Set<String> mainTypeCode = new HashSet<String>();
 
 	public HashMap<String, Object> headersForms(){		
 		HashMap<String, Object> headers = new HashMap<String,Object>();
@@ -34,44 +33,71 @@ public class API_AutoCommon {
 	}
 
 
-	public Response auto(String str, String str1) throws Exception{
+	public Response getAutoManufacturerDetails() throws Exception{
 		String endPoint = null;
-		String params = null; 
 		HashMap<String, Object> headers	= new HashMap<String,Object>();
 		headers = headersForms();
 		Response response = null;
+		RestAssured.baseURI = url;
+		endPoint = "v1/car-types/manufacturer?wa_key=coding-puzzle-client-449cc9d&locale=en";	
+		response = RestAssured.given().
+				when().log().all().headers(headers).get(endPoint);
 
-		if(str.equalsIgnoreCase("ManufacturerOfAuto")) {
-			RestAssured.baseURI = url;
-			endPoint = url_manufacturer;	
-			response = RestAssured.given().
-					when().log().all().headers(headers).get(endPoint);
-
-		}
-		
 		return response;
 	}
 
-	public Response validation_Auto(String str, Response resp) throws Exception{
+	public List<Response> getAutoMainTypeDetails() throws Exception{
 
-		boolean isMatching = false;
-		String status="";
-		String description="";
-		int statusCode = resp.getStatusCode();
-		JsonPath jsonPathEvaluator = resp.jsonPath();
-		if (str.equalsIgnoreCase("ManufacturerOfAuto")) {				
-			if(statusCode==200){
-				manufacturer_Map = jsonPathEvaluator.get("wkda");
-				manufacturer_code = manufacturer_Map.keySet();
-				isMatching = true;
-			}
+		String endPoint = null;
+		HashMap<String, Object> headers	= new HashMap<String,Object>();
+		headers = headersForms();
+		List<Response> mainTypeResponse = null;
+		RestAssured.baseURI = url;
+		Iterator<String> it = manufacturerCode.iterator();
+		while(it.hasNext()){
+			String manCode = it.next();
+			endPoint = "v1/car-types/manufacturer?wa_key=coding-puzzle-client-449cc9d&locale=en&manufacturer="+manCode;
+			Response response = RestAssured.given().
+					when().log().all().headers(headers).get(endPoint);
+			mainTypeResponse.add(response);
 		}
 
-		Assert.assertTrue(isMatching);
+		return mainTypeResponse;
+
+	}
+
+	public Response validateAutoManufacturer(Response resp) throws Exception{
+
+		boolean isMatching = false;
+		int statusCode = resp.getStatusCode();
+		JsonPath jsonPathEvaluator = resp.jsonPath();
+		Map<String,String> manufacturerMap = new HashMap<String,String>();
+		if (statusCode==200) {				
+			manufacturerMap = jsonPathEvaluator.get("wkda");
+			manufacturerCode = manufacturerMap.keySet();
+			isMatching = true;
+		}
+
+		Assert.assertTrue("Manufacturer Code is not matching",isMatching);
 		return resp;
 
 
 	}
 
-}
+	public List<Response> validateMainType(List<Response>resp) throws Exception{
+		boolean isMatching = false;
+		Map<String,String> mainTypeMap = new HashMap<String,String>();
+		for(Response r : resp){
+			JsonPath jsonPathEvaluator = r.jsonPath();
+			int statusCode = r.getStatusCode();
+			if (statusCode==200){
+				mainTypeMap = jsonPathEvaluator.get("wkda");
+				mainTypeCode = mainTypeMap.keySet();
+				isMatching = true;
+			}
+		}
+		Assert.assertTrue("Manufacturer Code is not matching",isMatching);
+		return resp;
 
+	}
+}
--- TechAssesment/src/test/java/stepDefinition/AutoManufactureApis.java
+++ TechAssesment/src/test/java/stepDefinition/AutoManufactureApis.java
@@ -1,5 +1,7 @@
 package stepDefinition;
 
+import java.util.List;
+
 import commonUtil.API_AutoCommon;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -7,25 +9,32 @@ import cucumber.api.java.en.When;
 import io.restassured.response.Response;
 
 public class AutoManufactureApis extends API_AutoCommon {
-	
-	
-   @Given("^user hits the the manufacturer get call and gets the manufacturer details$")
+
+
+	@Given("^user hits the the manufacturer get call and gets the manufacturer details$")
 	public void user_hits_the_the_manufacturer_get_call_and_gets_the_manufacturer_details() {
 		try{
-			Response res = auto("ManufacturerOfAuto", "");
-			validation_Auto("ManufacturerOfAuto", res);
+			Response res = getAutoManufacturerDetails();
+			validateAutoManufacturer(res);
 		}
 
 		catch(Exception e){
 			System.out.println(e.getMessage());
-			
+
 		}
 
 	}
 
 	@When("^user gets the manufacturer details and hits the car type get call$")
-	public void user_gets_the_manufacturer_details_and_hits_the_car_type_get_call() throws Exception {
+	public void user_gets_the_manufacturer_details_and_hits_the_car_type_get_call() {
 
+		try{
+			List<Response> mainTypeRes = getAutoMainTypeDetails();
+			validateMainType(mainTypeRes);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@Then("^user enters the manufacturer details and the main type details and hits the built in date$")
