4064fddd70dee1adfd638af8f32354c6ebb4a31d
Prakhar Chatterjee <prakharchatterjee@UE-B-NOTE-0052.local>
2021-01-09 16:24:56 +0530

    Adding CI/CD changes

---
+++ src/test/java/springRestServices/User.java
@@ -0,0 +1,33 @@
+package springRestServices;
+
+import static io.restassured.RestAssured.given;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import bin.UserBin;
+import config.RestAssuredConfig;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class User {
+
+	public RequestSpecification getUserDetailsApi() {
+
+		RequestSpecification requestSpecification = new RestAssuredConfig().getRequestSpecification();
+		requestSpecification.queryParam("username", System.getProperty("propertyName")).log().all();
+
+		return requestSpecification;
+
+	}
+
+	public UserBin getUserById(RequestSpecification specification, String endpoint) {
+
+	    Response response =  given().spec(specification).get(endpoint);
+		Assert.assertEquals(response.getStatusCode(),HttpStatus.SC_OK);
+        response.then().log().all();
+        UserBin[] userBin = response.as(UserBin[].class);
+
+		return userBin[0];
+
+	}
+	
+}
