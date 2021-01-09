03db771f7903d1d4d9697d78b4ba68121c4613dd
Prakhar Chatterjee <prakharchatterjee@UE-B-NOTE-0052.local>
2021-01-09 16:02:32 +0530

    Adding CI/CD changes

---
+++ src/test/java/springRestServices/Post.java
@@ -0,0 +1,49 @@
+package springRestServices;
+
+import static io.restassured.RestAssured.given;
+import java.util.List;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import com.codoid.products.exception.FilloException;
+import bin.PostBin;
+import common.Utility;
+import config.RestAssuredConfig;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Post {
+	
+	public RequestSpecification getUserPostsApi(int userId) {
+
+		RequestSpecification requestSpecification = new RestAssuredConfig().getRequestSpecification();
+		requestSpecification.queryParam("userId", userId).log().all();
+
+		return requestSpecification;
+
+	}
+
+	public PostBin[] getPostsByUserId(RequestSpecification specification, String endpoint) {
+
+	    Response response =  given().spec(specification).get(endpoint);
+		Assert.assertEquals(response.getStatusCode(),HttpStatus.SC_OK);
+        response.then().log().all();
+        PostBin[] postBin = response.as(PostBin[].class);
+
+		return postBin;
+
+	}
+	
+	public List<String> storePostBodies() throws FilloException {
+		Utility utility = new Utility();
+		List<String> bodies =  utility.readPostBody();
+		
+		return bodies;
+	}
+	
+	public List<String> storePostTitles() throws FilloException {
+		Utility utility = new Utility();
+		List<String> titles =  utility.readPostTitle();
+		
+		return titles;
+	}
+}
